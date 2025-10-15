3a98352bc24acb6d3083e14b9215bf5a475bbc6c
czy2373 <2311735466@qq.com>
2025-10-15 20:02:47 +0800

    docs: update _init_.py

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -8,6 +8,7 @@ from pcdet.utils import common_utils
 from .dataset import DatasetTemplate
 from .kitti.kitti_dataset import KittiDataset
 # from .nuscenes.nuscenes_dataset import NuScenesDataset
+from .wlr733.wlr733_dataset import WLR733Dataset
 from .waymo.waymo_dataset import WaymoDataset
 from .pandaset.pandaset_dataset import PandasetDataset
 from .lyft.lyft_dataset import LyftDataset
@@ -18,6 +19,7 @@ from .custom.custom_dataset import CustomDataset
 __all__ = {
     # 'DatasetTemplate': DatasetTemplate,
     'KittiDataset': KittiDataset,
+    'WLR733Dataset': WLR733Dataset,  
     # 'NuScenesDataset': NuScenesDataset,
     # 'WaymoDataset': WaymoDataset,
     # 'PandasetDataset': PandasetDataset,
